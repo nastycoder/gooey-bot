8544b30ab1ab0cdc05df5cde4cc5ee4654f67bc2
jasonmvand <54632931+jasonmvand@users.noreply.github.com>
2019-09-05 13:11:34 -0400

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -4,15 +4,13 @@ const filename = '../memory/deaths.json';
 function kill(msg, context){
 
   // Filter out bot mentions
-  const mentions = context.mentions.users.filter((user) {
-    return user !== bot.user;
-  });
+  const mentions = context.mentions.users.filter((user) => !bot.user);
   
   // If nobody else is mentioned in the message, quit here
   if (!mentions.length) return;
   
   // Increment the death counter for all mentioned members
-  function apendDeaths(memory, fd) {
+  function appendDeaths(memory) {
     // Iterate over mentioned users and increment their death counter
     mentions.forEach((value, key){
       if (value in memory) {
@@ -22,23 +20,23 @@ function kill(msg, context){
       }
     });
     // Write to memory and send a reply when finished
-    file.writeFile(fd, JSON.stringify(memory), (error) {
+    file.writeFile(filename, JSON.stringify(memory), (error) {
       // Send a reply after everything else is done
-      file.close(fd, () {
-        context.channel.send('You got it!');
-      });
+      context.channel.send('You got it!');
     })
   }
   
   // Try to open the memory file, if it fails assume that the file is fucked and start over
-  file.open(filename, 'a+', (error, fd) {
-    file.readFile(fd, (error, data){
+  file.readFile(filename, (error, data){
+    if (!error){
       try {
-        appendDeaths(JSON.parse(data), fd);
+        appendDeaths(JSON.parse(data));
       } catch (error) {
-        appendDeaths({}, fd);
+        appendDeaths({});
       }
-    });
+    } else {
+      appendDeaths({});
+    }
   });
   
 };
