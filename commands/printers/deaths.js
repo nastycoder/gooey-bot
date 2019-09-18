e8e707415d567c50cfa020743f3c403c9303ff02
jasonmvand <54632931+jasonmvand@users.noreply.github.com>
2019-09-18 11:45:32 -0400

    Update deaths.js
    sorting and inline display

--- commands/printers/deaths.js
+++ commands/printers/deaths.js
@@ -7,10 +7,17 @@ function printer(context) {
     let embed = new Discord.RichEmbed();
 
     let data = JSON.parse(fd);
+    let sorted = Object.entries(data).sort((a, b) => b[1].deaths > a[1].deaths);
+    /*
     for (id in data) {
       embed.addField(data[id].name, data[id].deaths);
     }
-
+    */
+    
+    for (id in sorted) { // [ [key, value], [key, value] ]
+      embed.addField(sorted[id][1].name, sorted[id][1].deaths, true /*inline*/);
+    }
+    
     embed.setTitle(`Current Death Counts`);
 
     return context.channel.send(embed);
